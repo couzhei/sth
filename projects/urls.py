0eb58fa6fba92be6f8399f282d137a1bcc21ad35
couzhei <couzhei@gmail.com>
2022-01-08 11:57:35 +0330

    templates created!

---
+++ projects/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [ # beware of the naming, it should be as it is or will throw error
+    path('', views.projects, name="projects"),
+    path('project/<str:pk>/', views.project, name="project"),
+]
