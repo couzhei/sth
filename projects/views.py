6ab489d1b6a8fd6b559487ec84f89871bf1246b3
couzhei <couzhei@gmail.com>
2022-01-11 05:51:02 +0330

    03 ended

--- projects/views.py
+++ projects/views.py
@@ -5,42 +5,58 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from projects.models import Project
+
 projectsList = [
     {
-        'id':'1',
+        'id': '1',
         'title': 'E-Commerce Website',
-        'description':'Fully functional ecommerce website'
+        'description': 'Fully functional ecommerce website'
     },
     {
-        'id':'2',
-        'title':'Portfolio Website',
-        'description':'This was a project where I built out my own portfolio'
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'This was a project where I built out my own portfolio'
     },
     {
-        'id':'3',
-        'title':'Social Network',
-        'description':'Awesome open source project I am still working on.'
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'Awesome open source project I am still working on.'
     }
 ]
 
 # writing these functions here is so impractical and nonstandard!
+
+
 def projects(request):
-    # return HttpResponse('Here are our products')
-    page = 'projects'
-    number = 10
-    context = {
-        'page':page,
-        'number':number,
-        'projects':projectsList
-    }
+    # # return HttpResponse('Here are our products')
+    # page = 'projects'
+    # number = 10
+    # context = {
+    #     'page':page,
+    #     'number':number,
+    #     'projects':projectsList
+    # }
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html',
-        context, # these are passed variables to jinja in html! REVOLUTIONARY!
-    )
- 
+                  context,  # these are passed variables to jinja in html! REVOLUTIONARY!
+                  )
+
+
 def project(request, pk):
-    # return HttpResponse(f'This is the project with id {pk}.')
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
-    return render(request, 'projects/single-project.html', {'project':projectObj})
+    # # # return HttpResponse(f'This is the project with id {pk}.')
+    # # projectObj = None
+    # # for i in projectsList:
+    # #     if i['id'] == pk:
+    # #         projectObj = i
+    # projectObj = Project.objects.get(id=pk)
+    # tags = projectObj.tags.all()
+    # return render(request, 'projects/single-project.html',
+    #               {'project': projectObj,
+    #               'tags':tags})
+    # The following works the same as above but instead we're gonna
+    # render the query right in the html page, check single-project.html
+    projectObj = Project.objects.get(id=pk)
+    return render(request, 'projects/single-project.html',
+                  {'project': projectObj})
--- queries.py
+++ queries.py
@@ -1,4 +1,4 @@
-# To test things out you can use `python manage.py shell`
+# To test things out you type `python manage.py shell` in django env
 
 # importing relevant module
 from django.shortcuts import get_object_or_404
@@ -24,11 +24,12 @@ projectObj = Project.objects.get(title='Portfolio Project')
 print(projectObj.created) # accessing attributes of that object
 
 # One line create and save
-Tag.objects.create(name="angular")
+Tag.objects.create(name="angular").save()
 
 # Create and Save
-user = User.objects.first()
-Project = Project(user=user, title='Item 1', price=19.95)
+item = Project.objects.create(title="Bumble Bee")
+item.vote_total = 40
+item.vote_ratio = 73
 # Save
 Project.save()
 
@@ -38,14 +39,10 @@ item.title = 'Bumble Lee'
 item.save()
 
 # Delete the most recently created object
-item = Project.objects.last()
+item = Project.objects.last() # I'm still confused about .first and .last
+# or
+item = Project.objects.get(title__startswith="Bum")
 item.delete()
-
-# Select Related (to avoid n+1 query)
-posts = Review.objects.select_related('user', 'category').all()
-
-# Read or render a 404 not found page
-project = get_object_or_404(Project, id=512)
 
 # Filter
 Project.objects.filter(vote_ratio__gt=2016)  # __gt = greater than
@@ -53,7 +50,6 @@ Project.objects.filter(vote_ratio__lt=2001)  # __lt = less than
 Project.objects.filter(vote_ratio__gt=80, title__endswith='e')
 Project.objects.filter(vote_ratio__gt=80, title__endswith='e')  # the inverse of the above
 Project.objects.filter(title__icontains='mB')  # __icontains
-
 
 # Filter on relationship sub model field
 Project.objects.get(user__usertitle='mike')
@@ -68,20 +64,8 @@ Project.objects.all().order_by('title')[0]
 # Slicing return last
 Project.objects.all().order_by('-title')[0]
 
-# Slicing limit/offset
-Project.objects.all().order_by('title')[1..10]
-
-# Updating
-project = Project.objects.first()
-project.title = 'new title'
-project.save()
-
 # One line update
 Project.objects.filter(id=4).update(title='new title')
-
-# Deleting
-project = Project.objects.first()
-project.delete()
 
 # One line delete
 Project.objects.get(id=1).delete()
@@ -90,24 +74,18 @@ Project.objects.get(id=1).delete()
 Project.objects.all().delete()
 
 # Query Models Children
-item = Project.objects.first()
+item = Project.objects.first() # we could take any object, this is just for simplicity
 # item.<childmodel>_set.all() the childmodel has to be lowercase
 item.review_set.all()
-
-# Set ForeignKey field value
-model1 = Model(title='dyson')
-Project.model = model1
-
-# Get ForeignKey value
-Project1.model.title
-model1.Project_set.all()
-
-# Add Many-to-Many
-Project1.tags.add(tag1)
-Project1.tags.all()
-tag1.Projects_set.all()
 
 
 # Query ManyToMany Fields
 item = Project.objects.first()
-item.relationship.all()
+# item.<relationship>.all()
+item.tags.all()
+
+# Select Related (to avoid n+1 query)
+posts = Review.objects.select_related('user', 'category').all()
+
+# Read or render a 404 not found page
+project = get_object_or_404(Project, id=512)
